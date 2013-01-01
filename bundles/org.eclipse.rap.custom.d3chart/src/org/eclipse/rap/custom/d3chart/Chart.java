0c9c2675358eba0104a581037fc97450aad6ddf4
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-01-01 21:33:22 +0100

    Setup projects (initial commit)

---
+++ bundles/org.eclipse.rap.custom.d3chart/src/org/eclipse/rap/custom/d3chart/Chart.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralf Sternberg - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.custom.d3chart;
+
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+
+public class Chart extends Canvas {
+
+  public Chart( Composite parent, int style ) {
+    super( parent, style );
+  }
+
+}
---
+++ tests/org.eclipse.rap.custom.d3chart.test/src/org/eclipse/rap/custom/d3chart/Chart_Test.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralf Sternberg - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.custom.d3chart;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.rap.rwt.testfixture.Fixture;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Chart_Test {
+
+  private Display display;
+  private Shell shell;
+
+  @Before
+  public void setUp() {
+    Fixture.setUp();
+    display = new Display();
+    shell = new Shell( display );
+  }
+
+  @Test
+  public void testCreate() {
+    Chart chart = new Chart( shell, SWT.NONE );
+
+    assertEquals( shell, chart.getParent() );
+  }
+
+}
