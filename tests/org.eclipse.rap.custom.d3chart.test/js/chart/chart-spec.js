5b855e24472b65aa8a9164d6c6ca85f368635729
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-08-03 21:04:40 +0200

    Add some basic jstests for chart

--- tests/org.eclipse.rap.custom.d3chart.test/js/chart/chart-spec.js
+++ tests/org.eclipse.rap.custom.d3chart.test/js/chart/chart-spec.js
@@ -8,34 +8,84 @@ describe( "Chart", function() {
   beforeEach( function() {
     rap.setup();
     parent = {
+      _listeners: {},
       append: function() {},
-      addListener: function() {},
-      getClientArea: function() { return [ 0, 0, 0, 0 ]; }
+      addListener: function( event, listener ) {
+        if( this._listeners[ event ] === undefined ) {
+          this._listeners[ event ] = [];
+        }
+        this._listeners[ event ].push( listener );
+      },
+      notify: function( event ) {
+        var listeners = this._listeners[ event ];
+        if( listeners ) {
+          for( var i = 0; i < listeners.length; i++ ) {
+            listeners[ i ]();
+          }
+        }
+      },
+      getClientArea: function() { return [ 20, 30, 200, 300 ]; }
     };
-    chart = new d3chart.Chart( parent );
     captor = [];
   } );
 
-  it( "has a DOM element", function() {
-    expect( chart.getElement() ).toBeDefined();
+  it( "creates a DOM element and appends it to its parent", function() {
+    spyOn( parent, "append" );
+
+    chart = new d3chart.Chart( parent );
+
+    expect( parent.append ).toHaveBeenCalled();
+    expect( parent.append.mostRecentCall.args[0] ).toBeDefined();
   } );
 
-  describe( "setType()", function() {
+  describe( "svg element", function() {
 
-    it( "changes type", function() {
-      chart.setType( "foo" );
-
-      expect( chart.getType() ).toEqual( "foo" );
+    beforeEach( function() {
+      chart = new d3chart.Chart( parent );
     } );
 
-    it( "schedules update", function() {
-      var called = false;
-      d3chart.Chart.renderers.foo = function() { called = true; };
+    it( "is created", function() {
+      expect( chart._svg.node().tagName ).toBe( "svg" );
+      expect( chart._svg.node().parentNode ).toBe( chart._element );
+    } );
 
-      chart.setType( "foo" );
+    it( "has initial size", function() {
+      expect( chart._svg.attr( "width" ) ).toBe( "200" );
+      expect( chart._svg.attr( "height" ) ).toBe( "300" );
+    } );
 
-      rap.notify( "render" );
-      expect( called ).toBe( true );
+    it( "is resized on parent resize", function() {
+      parent.getClientArea = function() { return [ 20, 30, 400, 600 ]; };
+
+      parent.notify( "Resize" );
+
+      expect( chart._svg.attr( "width" ) ).toBe( "400" );
+      expect( chart._svg.attr( "height" ) ).toBe( "600" );
+    } );
+
+  } );
+
+  describe( "getLayer", function() {
+
+    beforeEach( function() {
+      chart = new d3chart.Chart( parent );
+    } );
+
+    it( "returns a d3 selection", function() {
+      var layer = chart.getLayer( "foo" );
+
+      expect( typeof layer.data ).toBe( "function" );
+      expect( layer.length ).toBe( 1 );
+      expect( layer.node().tagName ).toBe( "g" );
+      expect( layer.classed( "foo" ) ).toBe( true );
+      expect( layer.node().parentNode ).toBe( chart._svg.node() );
+    } );
+
+    it( "returns selection with the same nodes for one name", function() {
+      var layer1 = chart.getLayer( "foo" );
+      var layer2 = chart.getLayer( "foo" );
+
+      expect( layer1.node() ).toBe( layer2.node() );
     } );
 
   } );
